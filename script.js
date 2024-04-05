e8c535e06ac0c5d8d4682fe94440179e166afbe6
Artur <artur,cheuchiev@gmail.com>
2024-04-05 15:36:30 +0200

    additions to the graph

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 window.onload = function () {
     let canvas = document.getElementById('canvas');
     let c = canvas.getContext("2d");
-
+    let input = document.querySelector('#input');
+    let draw = document.querySelector('#draw');
     function f(x) {
-        return x**x
+        return x**3 + 12 * x + 19; 
     }
-
+   
     let startX = -10;
     let endX = 10;
     let stepX = 0.1;
@@ -13,24 +14,39 @@ window.onload = function () {
     let minY = f(startX);
     let maxY = f(startX);
 
-    for(let x = startX + stepX; x <= endX; x+= stepX){
+    for (let x = startX + stepX; x <= endX; x += stepX) {
         let y = f(x);
         minY = Math.min(minY, y);
         maxY = Math.max(maxY, y);
     }
 
-  
     let scaleX = canvas.width / (endX - startX);
     let scaleY = canvas.height / (maxY - minY);
 
+    let middleY = canvas.height / 2;
+
+
+
     c.beginPath();
-    c.moveTo(startX * scaleX, canvas.height - f(startX) * scaleY);
-    for (let x = startX + stepX; x <= endX; x += stepX) {
+    for (let x = startX; x <= endX; x += stepX) {
         let y = f(x);
         let plotX = (x - startX) * scaleX;
-        let plotY = canvas.height - y * scaleY;
+        let plotY = middleY - y * scaleY;
         c.lineTo(plotX, plotY);
     }
+    c.strokeStyle = "blue";
+    c.stroke();
+
+    c.beginPath();
+    c.moveTo(0, middleY);
+    c.lineTo(canvas.width, middleY);
+    c.strokeStyle = "black";
+    c.stroke();
+
+
+    c.beginPath();
+    c.moveTo(canvas.width / 2, 0);
+    c.lineTo(canvas.width / 2, canvas.height);
+    c.strokeStyle = "black";
     c.stroke();
 }
-
